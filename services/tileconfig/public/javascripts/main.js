718622d87fe68c3032ae4e880759c4098489a520
Louie Ye <louie.ye@cnsdc01.pwc.com>
2013-12-03 16:01:47 +0800

    get Case Status from SFDC when configuration

--- services/tileconfig/public/javascripts/main.js
+++ services/tileconfig/public/javascripts/main.js
@@ -24,7 +24,7 @@ function doIt( host ) {
         if ( ticketID ) {
             toReturn['ticketID'] = ticketID;
         }
-        setTimeout(jive.tile.close(toReturn ), 4000);
+        setTimeout(function(){jive.tile.close(toReturn);}, 4000);
 
         // var query = encodeURIComponent("SELECT Id, Name, Description, StageName, Amount FROM Opportunity");
 // 
@@ -220,6 +220,7 @@ function casedoIt( host ) {
 function caselistdoIt( host ) {
 
     var oauth2SuccessCallback = function(ticketID) {
+        showCard("j-card-loading", 200);
 
         if ( !ticketID ) {
             $("#j-card-rejected .j-error").text("Unauthorized");
@@ -232,57 +233,83 @@ function caselistdoIt( host ) {
             return;
         }
 
+        var query = encodeURIComponent("Select MasterLabel, SortOrder from CaseStatus Order By SortOrder");
+
+        var config = onLoadContext['config'];
+        osapi.http.get({
+            'href' : host + '/sfdc/salesforce/query?' +
+                'id=' + ticketID +
+                "&ts=" + new Date().getTime() +
+                "&ticketID=" + ticketID +
+                "&query=" + query,
+            'format' : 'json',
+            'authz': 'signed'
+        }).execute(function( response ) {
+          if ( response.error ) {
+              $("#j-error").text(JSON.stringify(response.error, null, 4));
+              showCard("j-card-rejected");
+              return;
+          }
+          var data = response.content;
+
+          $.each( data.records, function() {
+              var record = $(this)[0];
+              var name = record["MasterLabel"];
+              var option = $("<option value=" + name + ">" + name + "</option>");
+              $("#select_status").append(option);
+          });
+          if ( config["select_status"] ) {
+              // set previously selected
+              $("#select_status").val( config["select_status"] );
+          }
+          showConfig();
+        });
+
+        var showConfig = function(){
+          showCard("j-card-configuration");
+          if ( config["select_priority"] ) {
+              // set previously selected
+              $("#select_priority").val( config["select_priority"] );
+          }
+
+          if ( config["select_order_by"] ) {
+              // set previously selected
+              $("#select_order_by").val( config["select_order_by"] );
+          }
+
+          if ( config["select_sort"] ) {
+              // set previously selected
+              $("#select_sort").val( config["select_sort"] );
+          }
+
+          if ( config["number_of_case"] ) {
+              // set previously selected
+              $("#number_of_case").val( config["number_of_case"] );
+          }
+
+          $("#btn_done").click( function() {
+              var select_status = $("#select_status").val();
+              var select_priority = $("#select_priority").val();
+              var select_order_by = $("#select_order_by").val();
+              var select_sort = $("#select_sort").val();
+              var number_of_case = $("#number_of_case").val();
+              var toReturn = {
+                  "select_status" : select_status,
+                  "select_priority" : select_priority,
+                  "select_order_by" : select_order_by,
+                  "select_sort" : select_sort,
+                  "number_of_case" : number_of_case,
+                  "isSFDC" : true
+              };
+              if ( ticketID ) {
+                  toReturn['ticketID'] = ticketID;
+              }
+
+              jive.tile.close(toReturn );
+          });
+        }
 
 
-            showCard("j-card-configuration");
-
-            var config = onLoadContext['config'];
-
-            if ( config["select_status"] ) {
-                // set previously selected
-                $("#select_status").val( config["select_status"] );
-            }
-
-            if ( config["select_priority"] ) {
-                // set previously selected
-                $("#select_priority").val( config["select_priority"] );
-            }
-
-            if ( config["select_order_by"] ) {
-                // set previously selected
-                $("#select_order_by").val( config["select_order_by"] );
-            }
-
-            if ( config["select_sort"] ) {
-                // set previously selected
-                $("#select_sort").val( config["select_sort"] );
-            }
-
-            if ( config["number_of_case"] ) {
-                // set previously selected
-                $("#number_of_case").val( config["number_of_case"] );
-            }
-
-            $("#btn_done").click( function() {
-                var select_status = $("#select_status").val();
-                var select_priority = $("#select_priority").val();
-                var select_order_by = $("#select_order_by").val();
-                var select_sort = $("#select_sort").val();
-                var number_of_case = $("#number_of_case").val();
-                var toReturn = {
-                    "select_status" : select_status,
-                    "select_priority" : select_priority,
-                    "select_order_by" : select_order_by,
-                    "select_sort" : select_sort,
-                    "number_of_case" : number_of_case,
-                    "isSFDC" : true
-                };
-                if ( ticketID ) {
-                    toReturn['ticketID'] = ticketID;
-                }
-
-                jive.tile.close(toReturn );
-            });
     };
 
     var jiveAuthorizeUrlErrorCallback = function(err) {
